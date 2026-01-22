7a9c37f18102f4fc1ea5f30b01f7bf4866ac3808
Peter Kinyua <peterkinyua13k@gmail.com>
2026-01-22 20:34:24 +0300

    first commit

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("hello")
+def get_full_name(first_name, last_name):
+    full_name = first_name.title() + " " + last_name.title()
+    return full_name
+
+
+print(get_full_name("peter", "kinyua"))  # Output: "Peter Kinyua"
+
+
+def get_name_with_age(name: str, age: int):
+    name_with_age = name + " is this old: " + str(age)
+    return name_with_age
+print(get_name_with_age("Peter Kinyua", 30))
+
+def get_items(item_a: str, item_b: int, item_c: float, item_d: bool, item_e: bytes):
+    return item_a, item_b, item_c, item_d, item_e
